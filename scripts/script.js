0c5fd1eb7ee158e64817c2ccad26be48ab4c131b
32 <32>
2025-03-26 12:01:09 +0300

    code update

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,13 @@
 import '../styles/main.css'
-import {cardsExample, createExampleCards } from './cardExampleFunc.js';
-
-
-const page = document.querySelector("body");
-const addButton = page.querySelector("#addButton");
-const cardTemplate = page.querySelector("#cardTemplate").content;
-const cardParent = page.querySelector("#cardsMainContainer");
-let warnNotif = page.querySelector("#warning");
-
-let cardsData = [];
+import { createExampleCards } from "./createCardScript/cardExampleFunc"
+import {cardTemplate, cardParent, addObjectToArray, cloneCardWithData} from "./createCardScript/cardFromInput"
+import { cardsExample } from './data/cardsData'
+import firstMenu from './cardEdit/firstMenu.js'
 
 createExampleCards(cardsExample, cardTemplate, cardParent)
 
-function addObjectToArray() {
-  let headingInput = page.querySelector("#heading").value;
-  let fileSelect = page.querySelector("#fileSelect").files[0];
-  let descriptionInput = page.querySelector("#description").value;
-
-  if (!headingInput || !descriptionInput || !fileSelect) {
-    warnNotif.textContent = "Пожалуйста, заполните все поля!";
-    return [];
-  }
-
-  let imageSrc = fileSelect ? URL.createObjectURL(fileSelect) : "";
- 
-  const newObject = {
-    title: headingInput,
-    imageSrc: imageSrc,
-    description: descriptionInput,
-  };
-
-  cardsData.unshift(newObject);
-
-  headingInput = page.querySelector("#heading").value = "";
-  fileSelect = page.querySelector("#fileSelect").value = ""; //данные все еще сохраняются
-  descriptionInput = page.querySelector("#description").value = "";
-  return cardsData
-}
-
-function cloneCardWithData(title, image, description) {
-  const cloneCard = cardTemplate.cloneNode(true);
-  cloneCard.querySelector(".card__heading").textContent = title;
-  cloneCard.querySelector(".card__image").src = image
-  cloneCard.querySelector(".card__description").textContent = description;
-
-      
-        const likeButton = cloneCard.querySelector(".card__button-static")
-        likeButton.addEventListener("click", () => {
-        likeButton.classList.toggle("like-pressed");
-        likeButton.classList.toggle("like-unpressed");
-      });
-      
-      cloneCard.querySelector('.delete-button').addEventListener('click', function() {
-        this.closest('.card__container').remove();
-      }); 
-      return cloneCard
-    }
-    
-addButton.addEventListener("click", function () {
+document.querySelector(".input__container").addEventListener("submit", function (e) {
+  e.preventDefault()
   const newCardData = addObjectToArray(); 
   if (newCardData.length < 1) {
     alert("Заполните все поля");
@@ -67,3 +17,17 @@ addButton.addEventListener("click", function () {
   
   cardParent.appendChild(newClonedCard);
 });
+
+
+cardParent.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card__button-static')) {
+    e.target.classList.toggle('like-pressed');
+    e.target.classList.toggle('like-unpressed');
+  }
+
+  if (e.target.classList.contains('delete-button')) {
+    e.target.closest('.card__container').remove();
+  }
+});
+
+firstMenu
