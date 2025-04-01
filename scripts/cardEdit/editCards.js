50feaafd6ac9aa9e6520302f0a471150e4fe3325
ierumi <stepastarostin@gmail.com>
2025-04-01 11:57:59 +0300

    add Functions not ready

--- scripts/cardEdit/editCards.js
+++ scripts/cardEdit/editCards.js
@@ -1,7 +1,28 @@
-export default document.getElementById('cardFormEditCard').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const editCardHeading = document.getElementById('editCardHeading').value
-    const editCardDescription = document.getElementById('editCardDescription').value
-    editCardHeading = e.target.closest('.card__heading').textContent
-    editCardDescription = e.target.closest('.card__description').textContent
-})
+
+export default document.body.addEventListener('mousedown', (e) => {
+    if (e.button === 2) {  
+      const cardContainer = e.target.closest('.card__container');
+      if (cardContainer) {
+        const editForm = document.getElementById('cardFormEditCard');
+        
+        const cardHeading = cardContainer.querySelector('.card__heading');
+        const cardDescription = cardContainer.querySelector('.card__description');
+          
+        const editCardHeading = document.getElementById('editCardHeading');
+        const editCardDescription = document.getElementById('editCardDescription');
+        editCardHeading.value = cardHeading.textContent
+        editCardDescription.value = cardDescription.textContent
+
+        editForm.addEventListener('submit', (e) => {
+          e.preventDefault();
+          
+          if (editCardHeading.value.length > 2 && editCardDescription.value.length > 2) {
+            cardHeading.textContent = editCardHeading.value;
+            cardDescription.textContent = editCardDescription.value;
+          } else {
+            alert ('мало текста')
+          }
+        const cardButtonContainer = cardContainer.querySelector('.card__button-container');
+        cardButtonContainer.classList.add('card__button-container-hide');
+      
+      })}}})
--- scripts/cardEdit/firstMenu.js
+++ scripts/cardEdit/firstMenu.js
@@ -1,23 +1,25 @@
 import { cardParent } from "../createCardScript/cardFromInput";
 
-export default document.body.addEventListener('mousedown', (e) => {
-    if (e.button === 2) {
-      const cardContainer = e.target.closest('.card__container');
-      if (cardContainer) {
-        const cardButtonContainer = cardContainer.querySelector('.card__button-container');
-        if (cardButtonContainer) {
-          if (cardButtonContainer.classList.contains('card__button-container-hide')) {
-            cardButtonContainer.classList.remove('card__button-container-hide');
-          } else {
-            cardButtonContainer.classList.add('card__button-container-hide');
-          }
+export default document.body.addEventListener("mousedown", (e) => {
+  if (e.button === 2) {
+    const cardContainer = e.target.closest(".card__container");
+    if (cardContainer) {
+      const cardButtonContainer = cardContainer.querySelector(
+        ".card__button-container"
+      );
+      if (cardButtonContainer) {
+        if (
+          cardButtonContainer.classList.contains("card__button-container-hide")
+        ) {
+          cardButtonContainer.classList.remove("card__button-container-hide");
+        } else {
+          cardButtonContainer.classList.add("card__button-container-hide");
         }
       }
     }
-  }
-  );
-  
-  cardParent.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-  });
 
+    cardParent.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
+  }
+});
